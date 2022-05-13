78f24a55d80b9b62eaee0d9e3ec1f24446eb762e
jaeC <thingbaijamjessikhuman@gmail.com>
2022-05-13 20:46:52 +0530

    left to replace columns in carousal with cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
@@ -38,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            padding: const EdgeInsets.only(top: 43, left: 23),
+            padding: const EdgeInsets.only(top: 86, left: 33, bottom: 33),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -46,37 +48,62 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
                 const Text(
                   "Your ",
-                  style: TextStyle(fontSize: 21),
+                  style: TextStyle(fontSize: 26),
                 ),
                 const Text(
                   "Cards",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
                 ),
               ],
             ),
           ),
           Container(
-            color: Colors.amber,
+            color: Colors.white,
             child: Column(
               children: [
                 CarouselSlider(
                   options: CarouselOptions(
-                    height: 100,
-                    aspectRatio: 3.1,
+                    enableInfiniteScroll: true,
+                    viewportFraction: 0.4,
+                    height: 220,
+                    aspectRatio: 16 / 9,
                   ),
                   items: [
-                    1,
-                    2,
-                    3,
+                    2574.08,
+                    133.86,
+                    223.87,
                   ].map((i) {
                     return Builder(
                       builder: (BuildContext context) {
-                        return Column(
-                          children: [
-                            const Icon(Icons.mail_outlined),
-                            const Text("****4092"),
-                            Text("slno. $i"),
-                          ],
+                        return Card(
+                          elevation: 5, borderOnForeground: true,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18)),
+                          color: Colors.red[400],
+                          child: Container(
+                            padding: EdgeInsets.all(18),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Icon(Icons.credit_card_rounded),
+                                    Text("****4092"),
+                                  ],
+                                ),
+                                Text(
+                                  "\$ $i",
+                                  style: TextStyle(
+                                      fontSize: 32,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+
+//
                         );
                       },
                     );
@@ -88,61 +115,216 @@ class _MyHomePageState extends State<MyHomePage> {
           Container(
             padding: const EdgeInsets.all(20),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text("Acivity"),
-                    Text(
-                      "History",
-                      style: TextStyle(backgroundColor: Colors.green),
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Text(
+                      "Activity",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    FlatButton(
+                      onPressed: null,
+                      child:
+                          Text('History', style: TextStyle(color: Colors.grey)),
+                      textColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                              color: Colors.grey,
+                              width: 1,
+                              style: BorderStyle.solid),
+                          borderRadius: BorderRadius.circular(50)),
                     )
                   ],
                 ),
-                const Text("06 June"),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Container(
+                    padding: EdgeInsets.only(top: 10),
+                    child: const Text(
+                      "06 June",
+                      style: TextStyle(color: Colors.grey, fontSize: 18),
+                    )),
+                ListView(
+                  shrinkWrap: true,
                   children: [
                     Row(
-                      children: const [
-                        Icon(Icons.food_bank_rounded),
-                        Text("McDonald's")
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.food_bank_rounded,
+                              size: 60,
+                              color: Colors.orange[300],
+                            ),
+                            Text(
+                              "McDonald's",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                        const Text("-\$ 5.99",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold))
                       ],
                     ),
-                    const Text("-\$ 5.99")
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
                     Row(
-                      children: const [
-                        Icon(Icons.health_and_safety_rounded),
-                        Text("Health insurance")
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.health_and_safety_rounded,
+                              size: 60,
+                              color: Colors.red[300],
+                            ),
+                            Text(
+                              "Health insurance",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                        const Text("-\$ 54.51",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold))
                       ],
                     ),
-                    const Text("-\$ 54.51")
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
                     Row(
-                      children: const [Icon(Icons.person), Text("transfer")],
-                    ),
-                    const Text("\$ 150")
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: const [
-                        Icon(Icons.receipt_rounded),
-                        Text("Bills")
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.account_circle,
+                              size: 60,
+                              color: Colors.grey[500],
+                            ),
+                            Text(
+                              "transfer",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                        const Text("\$ 150",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold))
                       ],
                     ),
-                    const Text("-\$ 400.00")
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.receipt_rounded,
+                              size: 60,
+                              color: Colors.blue[300],
+                            ),
+                            Text(
+                              "Bills",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                        const Text("-\$ 400.00",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.school_rounded,
+                              size: 60,
+                              color: Colors.green[300],
+                            ),
+                            Text(
+                              "School fee",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                        const Text("-\$ 123.25",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.shopping_cart,
+                              size: 60,
+                              color: Colors.purple[300],
+                            ),
+                            Text(
+                              "Shopping",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                        const Text("-\$ 54.32",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.agriculture_rounded,
+                              size: 60,
+                              color: Colors.brown[500],
+                            ),
+                            Text(
+                              "Vehicle servicing",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                        const Text("-\$ 1540.53",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.where_to_vote,
+                              size: 60,
+                              color: Colors.blue[300],
+                            ),
+                            Text(
+                              "Tour expenses",
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey),
+                            )
+                          ],
+                        ),
+                        const Text("-\$ 2660.00",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold))
+                      ],
+                    ),
                   ],
                 ),
               ],
