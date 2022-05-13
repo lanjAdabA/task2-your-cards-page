c1fbabb071db5447350dca8b3a0d95ffa7fcc4c8
jaeC <thingbaijamjessikhuman@gmail.com>
2022-05-13 12:33:30 +0530

    layout done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-// ignore: unused_import
 import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
@@ -43,6 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Container(
             padding: const EdgeInsets.only(top: 43, left: 23),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.start,
               // ignore: prefer_const_literals_to_create_immutables
               children: [
@@ -57,34 +55,99 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           ),
-          Column(
-            children: [
-              CarouselSlider(
-                options: CarouselOptions(
-                  height: 300,
-                  aspectRatio: 3.1,
-                ),
-                items: [
-                  1,
-                  2,
-                  3,
-                ].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Column(
-                        children: [
-                          const Icon(Icons.mail_outlined),
-                          const Text("****4092"),
-                          Text("slno. $i"),
-                        ],
-                      );
-                    },
-                  );
-                }).toList(),
-              )
-            ],
+          Container(
+            color: Colors.amber,
+            child: Column(
+              children: [
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: 100,
+                    aspectRatio: 3.1,
+                  ),
+                  items: [
+                    1,
+                    2,
+                    3,
+                  ].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Column(
+                          children: [
+                            const Icon(Icons.mail_outlined),
+                            const Text("****4092"),
+                            Text("slno. $i"),
+                          ],
+                        );
+                      },
+                    );
+                  }).toList(),
+                )
+              ],
+            ),
           ),
-          Row()
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text("Acivity"),
+                    Text(
+                      "History",
+                      style: TextStyle(backgroundColor: Colors.green),
+                    )
+                  ],
+                ),
+                const Text("06 June"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: const [
+                        Icon(Icons.food_bank_rounded),
+                        Text("McDonald's")
+                      ],
+                    ),
+                    const Text("-\$ 5.99")
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: const [
+                        Icon(Icons.health_and_safety_rounded),
+                        Text("Health insurance")
+                      ],
+                    ),
+                    const Text("-\$ 54.51")
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: const [Icon(Icons.person), Text("transfer")],
+                    ),
+                    const Text("\$ 150")
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: const [
+                        Icon(Icons.receipt_rounded),
+                        Text("Bills")
+                      ],
+                    ),
+                    const Text("-\$ 400.00")
+                  ],
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
@@ -94,6 +157,6 @@ class _MyHomePageState extends State<MyHomePage> {
        
         */
 
-/* 
+/*
 
           */
